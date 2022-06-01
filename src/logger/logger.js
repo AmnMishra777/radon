ce5af791287c8520013cee46e4a1e695ce326fef
AmnMishra777 <plot168@gmail.com>
2022-06-02 01:52:40 +0530

    assignment1

---
+++ src/logger/logger.js
@@ -0,0 +1,30 @@
+//const endpoint = "https://www.functionup.org"
+//const batch = "radon"
+
+//const welcome = "Welcome to my application. I am Aman and a part of FunctionUp radon cohort."
+
+// //const log = function() {
+//     console.log('I am inside the log function')
+// }
+
+ const welcome = function() {
+     console.log('Welcome to my application. I am Aman and a part of FunctionUp radon cohort')
+ }
+
+ 
+const problem1 = function() {
+    console.log('problem - 1')
+    }
+
+const empty = function(){
+    console.log(' ')
+}
+
+// module.exports.endpoint = endpoint
+// module.exports.batch = batch
+// module.exports.log = log
+module.exports.welcome = welcome
+module.exports.problem1 = problem1
+module.exports.empty = empty
+
+
