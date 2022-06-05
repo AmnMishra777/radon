fb2804118b1fd310bf0754ab2103fc702d2136af
AmnMishra777 <plot168@gmail.com>
2022-06-06 02:30:28 +0530

    mongo books assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const UserModel= require("../models/userModel")
+const UserModel= require("../models/userModel.js")
 
 const createUser= async function (req, res) {
     let data= req.body
@@ -11,5 +11,44 @@ const getUsersData= async function (req, res) {
     res.send({msg: allUsers})
 }
 
+let boooks = [{
+    "bookName": "Rich Dad Poor Dad",
+    "authorName": "Robert.K",
+    "category": "Finance",
+    "year": 1997
+},
+
+{  
+    "bookName": "Think and Grow Rich",
+    "authorName": "Napolean Hill",
+    "category": "Finance",
+    "year": 1937
+    
+},
+
+{ 
+    "bookName": "The Intelligent Investor",
+    "authorName": "Benjamin Graham",
+    "category": "Stocks and Finance",
+    "year": 1949
+}]
+
+
+const books= async function (req, res) {
+    let data= req.body
+//    let savedData=  await UserModel.create(data)
+//      let v = Obj[savedData].push(boooks)
+//     res.send( {data: v} )
+// }
+
+
+    let ele= req.body
+    boooks.push(ele)
+    res.send(  { data: boooks , status: true }  )
+
+}
+
+
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
+module.exports.books= books
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,32 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+
+// let books = [{
+//     "bookName": "Rich Dad Poor Dad",
+//     "authorName": "Robert.K",
+//     "category": "Finance",
+//     "year": 1997
+// },
+
+// {  
+//     "bookName": "Think and Grow Rich",
+//     "authorName": "Napolean Hill",
+//     "category": "Finance",
+//     "year": 1937
+    
+// },
+
+// { 
+//     "bookName": "The Intelligent Investor",
+//     "authorName": "Benjamin Graham",
+//     "category": "Stocks and Finance",
+//     "year": 1949
+// }]
+
+
+router.post("/bookss", UserController.books) 
+
 router.post("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
