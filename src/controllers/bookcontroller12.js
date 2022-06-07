7e746a5afb97b97ef091b6000a0ea761ca7e7065
AmnMishra777 <plot168@gmail.com>
2022-06-08 03:22:10 +0530

    crud operations mongodb

---
+++ src/controllers/bookcontroller12.js
@@ -0,0 +1,48 @@
+const { count } = require("console")
+const BookModel= require("../models/bookModel.js")
+const AuthorModel= require("../models/authorModel.js")
+
+const createAuthor= async function (req, res) {
+    let data= req.body
+
+    let savedData= await AuthorModel.create(data)
+    res.send({msg: savedData})
+}
+const createBook= async function (req, res) {
+    let data= req.body
+    let savedData= await BookModel.create(data)
+    res.send({msg: savedData})
+}
+const getBooksbyChetanBhagat= async function (req, res) {
+    let data= await AuthorModel.find({author_name:"Chetan Bhagat"}).select("author_id")
+    let bookData = await BookModel.find({author_id:data[0].author_id})
+  res.send({msg:bookData})
+}
+
+
+const authorofBook= async function (req, res) {
+  let data=await BookModel.findOneAndUpdate({name:"two states"},{$set:{prices:100}},{new:true})
+  let authorData=await AuthorModel.find({author_id:data.author_id}.select("author_name"))
+  let price= data.prices
+     
+     res.send( { msg: authorData,price})
+}
+
+
+
+
+
+
+
+// CRUD OPERATIONS:
+// CREATE
+// READ
+// UPDATE
+// DELETE
+
+
+
+module.exports.createAuthor= createAuthor
+module.exports.createBook= createBook
+module.exports.getBooksbyChetanBhagat= getBooksbyChetanBhagat
+module.exports.authorofBook= authorofBook
